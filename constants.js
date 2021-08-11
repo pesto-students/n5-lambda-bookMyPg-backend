a4103d493114256e334760fd7b6a3d2e363b8596
haridhole <hari.dhole@circles.asia>
2021-08-11 21:42:26 +0530

    refactored date fields to from_date and to_date

--- constants.js
+++ constants.js
@@ -1,27 +1,27 @@
 module.exports = {
-  SENDER_EMAIL: 'BookMyPG<bookmypg.n5lambda@gmail.com>',
-  RENT_FILTER_CONVENTIONS: [
-    {
-      greater_than: 0,
-      less_than: 10000,
-    },
-    {
-      greater_than: 10000,
-      less_than: 15000,
-    },
-    {
-      greater_than: 15000,
-    },
-  ],
-  POPULATE_USER_FIELDS: ['firstName', 'lastName'],
-  POPULATE_AMENITY_FIELDS: ['name', 'logo'],
-  POPULATE_LOCATION_FIELDS: ['name'],
-  POPULATE_PROPERTY_FIELDS: ['name'],
-  POPULATE_REVIEW_FIELDS: ['rating', 'description', 'createdAt'],
-  EMAIL_TEMPLATE_TEXT: {
-    Visit: 'visitTemplate',
-    Userrating: 'userratingTemplate',
-    Propertyrating: 'propertyratingTemplate',
-    Query: 'queryTemplate',
-  },
+	SENDER_EMAIL: "BookMyPG<bookmypg.n5lambda@gmail.com>",
+	RENT_FILTER_CONVENTIONS: [
+		{
+			greater_than: 0,
+			less_than: 10000,
+		},
+		{
+			greater_than: 10000,
+			less_than: 15000,
+		},
+		{
+			greater_than: 15000,
+		},
+	],
+	POPULATE_USER_FIELDS: ["firstName", "lastName", "email"],
+	POPULATE_AMENITY_FIELDS: ["name", "logo"],
+	POPULATE_LOCATION_FIELDS: ["name"],
+	POPULATE_PROPERTY_FIELDS: ["name"],
+	POPULATE_REVIEW_FIELDS: ["rating", "description", "createdAt"],
+	EMAIL_TEMPLATE_TEXT: {
+		Visit: "visitTemplate",
+		Userrating: "userratingTemplate",
+		Propertyrating: "propertyratingTemplate",
+		Query: "queryTemplate",
+	},
 };
