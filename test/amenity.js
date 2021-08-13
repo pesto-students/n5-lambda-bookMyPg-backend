4b194b2a14e68f7e13005b621a213f61e3e76cc2
Monali <monalidoshi9@gmail.com>
2021-08-13 18:25:58 +0530

    Remove unused imports

--- test/amenity.js
+++ test/amenity.js
@@ -1,6 +1,5 @@
-const { chai, server, should } = require('./testConfig');
-const AmenityModel = require('../models/amenityModel');
-const constants = require('./constants');
+const { chai, server } = require("./testConfig");
+const constants = require("./constants");
 
 /**
  * Test cases to test all the amenity APIs
@@ -13,109 +12,109 @@ const constants = require('./constants');
  * (6) Disable Amenity
  */
 
-describe('Amenity', () => {
-  const userTestData = {};
-  const testData = constants.AMENITY_TEST_DATA;
+describe("Amenity", () => {
+	const userTestData = {};
+	const testData = constants.AMENITY_TEST_DATA;
 
-  /*
+	/*
    * Test the /GET Auth-token route
    */
-  describe('/GET Auth-token', () => {
-    it('it should do user Login for Amenity', done => {
-      chai
-        .request(server)
-        .get('/api/users/user/' + process.env.TEST_CASE_EMAIL)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          userTestData.token = res.body.data.token;
-          done();
-        });
-    });
-  });
-  /*
+	describe("/GET Auth-token", () => {
+		it("it should do user Login for Amenity", done => {
+			chai
+				.request(server)
+				.get("/api/users/user/" + process.env.TEST_CASE_EMAIL)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					userTestData.token = res.body.data.token;
+					done();
+				});
+		});
+	});
+	/*
    * Test the /POST route
    */
-  describe('/POST Amenity Store', () => {
-    it('It should store amenity', done => {
-      chai
-        .request(server)
-        .post('/api/amenities')
-        .send(testData)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
-  /*
+	describe("/POST Amenity Store", () => {
+		it("It should store amenity", done => {
+			chai
+				.request(server)
+				.post("/api/amenities")
+				.send(testData)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
+	/*
    * Test the /GET route
    */
-  describe('/GET All amenities', () => {
-    it('it should GET all the amenities', done => {
-      chai
-        .request(server)
-        .get('/api/amenities')
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          testData._id = res.body.data[0]._id;
-          done();
-        });
-    });
-  });
+	describe("/GET All amenities", () => {
+		it("it should GET all the amenities", done => {
+			chai
+				.request(server)
+				.get("/api/amenities")
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					testData._id = res.body.data[0]._id;
+					done();
+				});
+		});
+	});
 
-  /*
+	/*
    * Test the /GET/:id route
    */
-  describe('/GET/:id amenity', () => {
-    it('it should GET the amenity', done => {
-      chai
-        .request(server)
-        .get('/api/amenities/' + testData._id)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
-  /*
+	describe("/GET/:id amenity", () => {
+		it("it should GET the amenity", done => {
+			chai
+				.request(server)
+				.get("/api/amenities/" + testData._id)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
+	/*
    * Test the /PUT/:id route
    */
-  describe('/PUT/:id amenity', () => {
-    it('it should PUT the amenities', done => {
-      chai
-        .request(server)
-        .put('/api/amenities/' + testData._id)
-        .send(testData)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
+	describe("/PUT/:id amenity", () => {
+		it("it should PUT the amenities", done => {
+			chai
+				.request(server)
+				.put("/api/amenities/" + testData._id)
+				.send(testData)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
 
-  /*
+	/*
    * Test the /DELETE/:id route
    */
-  describe('/DELETE/:id amenity', () => {
-    it('it should DELETE the amenities', done => {
-      chai
-        .request(server)
-        .delete('/api/amenities/' + testData._id)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
+	describe("/DELETE/:id amenity", () => {
+		it("it should DELETE the amenities", done => {
+			chai
+				.request(server)
+				.delete("/api/amenities/" + testData._id)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
 });
--- test/complaint.js
+++ test/complaint.js
@@ -1,6 +1,5 @@
-const { chai, server, should } = require('./testConfig');
-const ComplaintModel = require('../models/complaintModel');
-const constants = require('./constants');
+const { chai, server } = require("./testConfig");
+const constants = require("./constants");
 
 /**
  * Test cases to test all the complaint APIs
@@ -13,108 +12,108 @@ const constants = require('./constants');
  * (6) Update Complaint
  */
 
-describe('Complaint', () => {
-  const userTestData = {};
-  const testData = constants.COMPLAINT_TEST_DATA;
+describe("Complaint", () => {
+	const userTestData = {};
+	const testData = constants.COMPLAINT_TEST_DATA;
 
-  /*
+	/*
    * Test the /GET Auth-token route
    */
-  describe('/GET Auth-token', () => {
-    it('it should do user Login for Complaint', done => {
-      chai
-        .request(server)
-        .get('/api/users/user/' + process.env.TEST_CASE_EMAIL)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          userTestData.token = res.body.data.token;
-          done();
-        });
-    });
-  });
-  /*
+	describe("/GET Auth-token", () => {
+		it("it should do user Login for Complaint", done => {
+			chai
+				.request(server)
+				.get("/api/users/user/" + process.env.TEST_CASE_EMAIL)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					userTestData.token = res.body.data.token;
+					done();
+				});
+		});
+	});
+	/*
    * Test the /POST route
    */
-  describe('/POST Complaint Store', () => {
-    it('It should store complaint', done => {
-      chai
-        .request(server)
-        .post('/api/complaints')
-        .send(testData)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
-  /*
+	describe("/POST Complaint Store", () => {
+		it("It should store complaint", done => {
+			chai
+				.request(server)
+				.post("/api/complaints")
+				.send(testData)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
+	/*
    * Test the /GET route
    */
-  describe('/GET All complaints', () => {
-    it('it should GET all the complaints', done => {
-      chai
-        .request(server)
-        .get('/api/complaints')
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          testData._id = res.body.data[0]._id;
-          done();
-        });
-    });
-  });
+	describe("/GET All complaints", () => {
+		it("it should GET all the complaints", done => {
+			chai
+				.request(server)
+				.get("/api/complaints")
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					testData._id = res.body.data[0]._id;
+					done();
+				});
+		});
+	});
 
-  /*
+	/*
    * Test the /GET/:id route
    */
-  describe('/GET/:id complaint', () => {
-    it('it should GET the complaint', done => {
-      chai
-        .request(server)
-        .get('/api/complaints/' + testData._id)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
-  /*
+	describe("/GET/:id complaint", () => {
+		it("it should GET the complaint", done => {
+			chai
+				.request(server)
+				.get("/api/complaints/" + testData._id)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
+	/*
    * Test the /GET/owner/:id route
    */
-  describe('/GET/:id complaint', () => {
-    it('it should GET the complaint', done => {
-      chai
-        .request(server)
-        .get('/api/complaints/owner/' + testData.owner)
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
-  /*
+	describe("/GET/:id complaint", () => {
+		it("it should GET the complaint", done => {
+			chai
+				.request(server)
+				.get("/api/complaints/owner/" + testData.owner)
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
+	/*
    * Test the /PUT/:id route
    */
-  describe('/PUT/:id complaint', () => {
-    it('it should PUT the complaints', done => {
-      chai
-        .request(server)
-        .put('/api/complaints/' + testData._id)
-        .send({ status: 'Resolved' })
-        .set('x-auth-token', userTestData.token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message').eql('Operation Success');
-          done();
-        });
-    });
-  });
+	describe("/PUT/:id complaint", () => {
+		it("it should PUT the complaints", done => {
+			chai
+				.request(server)
+				.put("/api/complaints/" + testData._id)
+				.send({ status: "Resolved" })
+				.set("x-auth-token", userTestData.token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property("message").eql("Operation Success");
+					done();
+				});
+		});
+	});
 });
