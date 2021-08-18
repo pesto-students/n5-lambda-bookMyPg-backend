5a4d27e7c39678aeb510c9404f3056ec6b103eed
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-18 15:34:14 +0530

    Delete complaintschart.js

--- routes/complaintschart.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const complaintschartController = require('../controllers/complaintschartController');
-
-var router = express.Router();
-
-router.get('/', complaintschartController.complaintsList);
-
-module.exports = router;
