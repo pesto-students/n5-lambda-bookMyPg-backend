28454e588697230460b96177551912e660476a08
haridhole <hari.dhole@circles.asia>
2021-08-21 16:47:35 +0530

    added image to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,22 +1,23 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: Number },
-    role: {
-      type: String,
-      enum: ['admin', 'user', 'owner'],
-      required: true,
-      default: 'user',
-    },
-    property: { type: mongoose.Types.ObjectId, ref: 'property' },
-    onboardedAt: { type: Date },
-    isactive: { type: Boolean, required: true, default: 1 },
-  },
-  { timestamps: true },
+	{
+		firstName: { type: String, required: true },
+		lastName: { type: String, required: true },
+		email: { type: String, required: true },
+		phone: { type: Number },
+		role: {
+			type: String,
+			enum: ["admin", "user", "owner"],
+			required: true,
+			default: "user",
+		},
+		image: { type: String, required: true },
+		property: { type: mongoose.Types.ObjectId, ref: "property" },
+		onboardedAt: { type: Date },
+		isactive: { type: Boolean, required: true, default: 1 },
+	},
+	{ timestamps: true },
 );
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model("user", UserSchema);
